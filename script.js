2451bd6687866dac4ee217ac25c8843fe972646f
JoshuaBulyanZebua <joshuabulyan24@gmail.com>
2023-10-20 20:36:15 +0700

    menambahkan fungsi pencarian

--- script.js
+++ script.js
@@ -61,90 +61,63 @@ async function ambilData() {
           kelas === e.target.value ? 'block' : 'none';
       }
     });
+
+    // Membuat fungsi search
+    const searchInput = document.getElementById('cari');
+
+    searchInput.addEventListener('keyup', (e) => {
+      const keyword = e.target.value.toLowerCase();
+      const dataSiswaFiltered = dataSiswa.filter((item) => {
+        return (
+          item.nama.toLowerCase().includes(keyword) ||
+          item.nisn.toLowerCase().includes(keyword) ||
+          item.alamat.toLowerCase().includes(keyword)
+        );
+      });
+      const dataSiswaElements = {
+        X1: document.getElementById('dataSiswaX1'),
+        X2: document.getElementById('dataSiswaX2'),
+        X3: document.getElementById('dataSiswaX3'),
+        XI1: document.getElementById('dataSiswaXI1'),
+        XI2: document.getElementById('dataSiswaXI2'),
+        XI3: document.getElementById('dataSiswaXI3'),
+        XII1: document.getElementById('dataSiswaXII1'),
+        XII2: document.getElementById('dataSiswaXII2'),
+        XII3: document.getElementById('dataSiswaXII3'),
+      };
+
+      for (const kelas in dataSiswaElements) {
+        dataSiswaElements[kelas].innerHTML = '';
+        const dataSiswaKelas = dataSiswaFiltered.filter(
+          (item) => item.kelas === kelas
+        );
+
+        dataSiswaKelas.forEach((item) => {
+          dataSiswaElements[kelas].innerHTML += `
+            Data siswa ditemukan
+            <br>
+            <table border = "1">
+            <tr>
+              <th>Kelas</th>
+              <th>Nama</th>
+              <th>NISN</th>
+              <th>Jenis Kelamin</th>
+              <th>Alamat</th>
+            </tr>
+            <tr>
+              <td>${item.kelas}</td>
+              <td>${item.nama}</td>
+              <td>${item.nisn}</td>
+              <td>${item.jenisKelamin}</td>
+              <td>${item.alamat}</td>
+              </tr>
+            </table>`;
+        });
+      }
+    });
   } catch (error) {
     console.log('Error in ambilData', error);
   }
 }
 
 ambilData(); // Call the function to start the process
-
-// Pencarian
-const cari = document.getElementById('cari');
-
-cari.addEventListener('keyup', (e) => {
-  const value = e.target.value.toLowerCase();
-  const dataSiswaX = dataSiswa.filter((item) => item.kelas === 'X');
-  const dataSiswaXI = dataSiswa.filter((item) => item.kelas === 'XI');
-  const dataSiswaXII = dataSiswa.filter((item) => item.kelas === 'XII');
-
-  if (kelas.value === 'X') {
-    tableX.innerHTML = `
-                    <tr>
-                        <th>Kelas</th>
-                        <th>Nama</th>
-                        <th>NISN</th>
-                        <th>Jenis Kelamin</th>
-                        <th>Alamat</th>
-                    </tr>
-                `;
-    dataSiswaX.forEach((item) => {
-      if (item.nama.toLowerCase().includes(value)) {
-        tableX.innerHTML += `
-                        <tr>
-    
-                            <td>${item.nama}</td>
-                            <td>${item.nisn}</td>
-                            <td>${item.jenisKelamin}</td>
-                            <td>${item.alamat}</td>
-                        </tr>
-                    `;
-      }
-    });
-  } else if (kelas.value === 'XI') {
-    tableXI.innerHTML = `
-                    <tr>
-                        <th>Kelas</th>
-                        <th>Nama</th>
-                        <th>NISN</th>
-                        <th>Jenis Kelamin</th>
-                        <th>Alamat</th>
-                    </tr>
-                `;
-    dataSiswaXI.forEach((item) => {
-      if (item.nama.toLowerCase().includes(value)) {
-        tableXI.innerHTML += `
-                        <tr>
-    
-                            <td>${item.nama}</td>
-                            <td>${item.nisn}</td>
-                            <td>${item.jenisKelamin}</td>
-                            <td>${item.alamat}</td>
-                        </tr>
-                    `;
-      }
-    });
-  } else if (kelas.value === 'XII') {
-    tableXII.innerHTML = `
-                    <tr>
-                        <th>Kelas</th>
-                        <th>Nama</th>
-                        <th>NISN</th>
-                        <th>Jenis Kelamin</th>
-                        <th>Alamat</th>
-                    </tr>
-                `;
-    dataSiswaXII.forEach((item) => {
-      if (item.nama.toLowerCase().includes(value)) {
-        tableXII.innerHTML += `
-                        <tr>
-    
-                            <td>${item.nama}</td>
-                            <td>${item.nisn}</td>
-                            <td>${item.jenisKelamin}</td>
-                            <td>${item.alamat}</td>
-                        </tr>
-                    `;
-      }
-    });
-  }
-});
